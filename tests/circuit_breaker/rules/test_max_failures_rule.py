469f6cee54823659a8d7e7edaf22a804be9ae5d9
Diego Garcia <drgarcia1986@gmail.com>
2019-12-13 18:04:43 -0300

    Simplify max faliures rule tests

--- tests/circuit_breaker/rules/test_max_failures_rule.py
+++ tests/circuit_breaker/rules/test_max_failures_rule.py
@@ -15,20 +15,17 @@ class TestMaxFailuresRule:
     def test_max_failures_rule_should_return_rule_instance(self, rule):
         assert isinstance(rule, BaseRule)
 
-    def test_should_not_open_circuit(self, rule):
+    @pytest.mark.parametrize('failures,expected', (
+        (1, False),
+        (5, False),
+        (10, True),
+        (11, True)
+    ))
+    def test_should_check_circuit_state(self, rule, failures, expected):
         assert rule.should_open_circuit(
-            total_failures=5,
-            total_requests=5
-        ) is False
-
-    def test_should_open_circuit(self, rule):
-        assert rule.should_open_circuit(
-            total_failures=20,
-            total_requests=5
-        ) is True
-
-    def test_should_increase_failure_count(self, rule):
-        assert rule.should_increase_failure_count() is True
+            total_failures=failures,
+            total_requests=0
+        ) is expected
 
     def test_should_not_increase_request_count(self, rule):
         assert rule.should_increase_request_count() is False
