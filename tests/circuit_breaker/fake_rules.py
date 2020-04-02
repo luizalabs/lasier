e74ef48f497ac95a726edd2da4d8dcf05733181b
Diego Garcia <drgarcia1986@gmail.com>
2020-04-02 19:09:15 -0300

    Add more verifications on fake rules to prevent silence error on cb implementation

--- tests/circuit_breaker/fake_rules.py
+++ tests/circuit_breaker/fake_rules.py
@@ -1,43 +1,56 @@
 from lasier.circuit_breaker.rules.base import BaseRule
 
 
-class ShouldNotOpenRule(BaseRule):
+class _BaseFakeRule(BaseRule):
 
     def should_open_circuit(self, total_failures, total_requests):
+        assert isinstance(
+            total_failures, int
+        ), f'total_failures, expected int got {type(total_failures)}'
+        assert isinstance(
+            total_requests, int
+        ), f'total_requests, expected int got {type(total_requests)}'
         return False
+
+
+class ShouldNotOpenRule(_BaseFakeRule):
+
+    def should_open_circuit(self, total_failures, total_requests):
+        return super().should_open_circuit(total_failures, total_requests)
 
     def log_increase_failures(self, total_failures, total_requests):
         pass
 
 
-class ShouldOpenRule(BaseRule):
+class ShouldOpenRule(_BaseFakeRule):
 
     def should_open_circuit(self, total_failures, total_requests):
+        super().should_open_circuit(total_failures, total_requests)
         return True
 
     def log_increase_failures(self, total_failures, total_requests):
         pass
 
 
-class ShouldNotIncreaseFailureRule(BaseRule):
+class ShouldNotIncreaseFailureRule(_BaseFakeRule):
 
     def should_increase_failure_count(self):
         return False
 
     def should_open_circuit(self, total_failures, total_requests):
-        return False
+        return super().should_open_circuit(total_failures, total_requests)
 
     def log_increase_failures(self, total_failures, total_requests):
         pass
 
 
-class ShouldNotIncreaseRequestRule(BaseRule):
+class ShouldNotIncreaseRequestRule(_BaseFakeRule):
 
     def should_increase_request_count(self):
         return False
 
     def should_open_circuit(self, total_failures, total_requests):
-        return False
+        return super().should_open_circuit(total_failures, total_requests)
 
     def log_increase_failures(self, total_failures, total_requests):
         pass
