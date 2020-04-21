bfdf019c25c68d35b65f7cb7f01e12e66da85c26
Diego Garcia <drgarcia1986@gmail.com>
2020-04-21 12:43:13 -0300

    Add typing on circuit breaker

--- lasier/circuit_breaker/base.py
+++ lasier/circuit_breaker/base.py
@@ -1,5 +1,8 @@
 import inspect
 import logging
+from typing import Any, Iterable, Optional, Type
+
+from .rules.base import BaseRule
 
 logger = logging.getLogger(__name__)
 
@@ -8,13 +11,13 @@ class CircuitBreakerBase:
 
     def __init__(
         self,
-        rule,
-        cache,
-        failure_exception,
-        failure_timeout=None,
-        circuit_timeout=None,
-        catch_exceptions=None,
-    ):
+        rule: BaseRule,
+        cache: Any,
+        failure_exception: Type[Exception],
+        failure_timeout: Optional[float] = None,
+        circuit_timeout: Optional[float] = None,
+        catch_exceptions: Optional[Iterable[Type[Exception]]] = None,
+    ) -> None:
         self.rule = rule
         self.cache = cache
         self.failure_timeout = failure_timeout
@@ -23,17 +26,17 @@ class CircuitBreakerBase:
         self.failure_exception = failure_exception
         self.catch_exceptions = catch_exceptions or (Exception,)
 
-    def _is_catchable_exception(self, exception):
+    def _is_catchable_exception(self, exception: Type[Exception]) -> bool:
         return inspect.isclass(exception) and any(
             issubclass(exception, exception_class)
             for exception_class in self.catch_exceptions
         )
 
-    def _notify_open_circuit(self):
+    def _notify_open_circuit(self) -> None:
         logger.critical(
             f'Open circuit for {self.rule.failure_cache_key} '
             f'{self.circuit_cache_key}'
         )
 
-    def _notify_max_failures_exceeded(self):
+    def _notify_max_failures_exceeded(self) -> None:
         logger.info(f'Max failures exceeded by: {self.rule.failure_cache_key}')
