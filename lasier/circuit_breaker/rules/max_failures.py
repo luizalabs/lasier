f4ceb6693898b7bb69b1dc3ea6220af968594513
Diego Garcia <drgarcia1986@gmail.com>
2020-04-21 12:18:53 -0300

    Add typing on rules

--- lasier/circuit_breaker/rules/max_failures.py
+++ lasier/circuit_breaker/rules/max_failures.py
@@ -7,16 +7,20 @@ logger = logging.getLogger(__name__)
 
 class MaxFailuresRule(BaseRule):
 
-    def __init__(self, max_failures, failure_cache_key):
+    def __init__(self, max_failures: int, failure_cache_key: str) -> None:
         super().__init__(failure_cache_key=failure_cache_key)
         self.max_failures = max_failures
 
-    def should_open_circuit(self, total_failures, total_requests):
+    def should_open_circuit(
+        self, total_failures: int, total_requests: int
+    ) -> bool:
         return total_failures >= self.max_failures
 
-    def log_increase_failures(self, total_failures, total_requests):
+    def log_increase_failures(
+        self, total_failures: int, total_requests: int
+    ) -> None:
         logger.info(
-            f'Increase failure for: {self.failures_cache_key} - '
+            f'Increase failure for: {self.failure_cache_key} - '
             f'max failures {self.max_failures} - '
             f'total requests {total_requests} - '
             f'total failures {total_failures}'
