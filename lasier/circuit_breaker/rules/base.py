3ddbe53117637cdfc0d43ca6ccf62057425a146e
Diego Garcia <drgarcia1986@gmail.com>
2020-04-21 12:18:53 -0300

    Add typing on rules

--- lasier/circuit_breaker/rules/base.py
+++ lasier/circuit_breaker/rules/base.py
@@ -1,22 +1,29 @@
 import abc
+from typing import Optional
 
 
 class BaseRule(metaclass=abc.ABCMeta):
 
-    def __init__(self, failure_cache_key, request_cache_key=None):
+    def __init__(
+        self, failure_cache_key: str, request_cache_key: Optional[str] = None
+    ) -> None:
         self.failure_cache_key = failure_cache_key
         self.request_cache_key = request_cache_key
 
     @abc.abstractmethod
-    def should_open_circuit(self, total_failures, total_requests):
+    def should_open_circuit(
+        self, total_failures: int, total_requests: int
+    ) -> bool:
         pass
 
-    def should_increase_failure_count(self):
+    def should_increase_failure_count(self) -> bool:
         return self.failure_cache_key is not None
 
-    def should_increase_request_count(self):
+    def should_increase_request_count(self) -> bool:
         return self.request_cache_key is not None
 
     @abc.abstractmethod
-    def log_increase_failures(self, total_failures, total_requests):
+    def log_increase_failures(
+        self, total_failures: int, total_requests: int
+    ) -> None:
         pass
--- lasier/circuit_breaker/rules/percentage_failures.py
+++ lasier/circuit_breaker/rules/percentage_failures.py
@@ -9,11 +9,11 @@ class PercentageFailuresRule(BaseRule):
 
     def __init__(
         self,
-        max_failures_percentage,
-        failure_cache_key,
-        min_accepted_requests,
-        request_cache_key,
-    ):
+        max_failures_percentage: int,
+        failure_cache_key: str,
+        min_accepted_requests: int,
+        request_cache_key: str,
+    ) -> None:
         super().__init__(
             failure_cache_key=failure_cache_key,
             request_cache_key=request_cache_key,
@@ -21,12 +21,16 @@ class PercentageFailuresRule(BaseRule):
         self.max_failures_percentage = max_failures_percentage
         self.min_accepted_requests = min_accepted_requests
 
-    def _get_percentage_failures(self, total_failures, total_requests):
+    def _get_percentage_failures(
+        self, total_failures: int, total_requests: int
+    ) -> float:
         if total_requests > 0:
             return (total_failures * 100) / total_requests
         return 0
 
-    def should_open_circuit(self, total_failures, total_requests):
+    def should_open_circuit(
+        self, total_failures: int, total_requests: int
+    ) -> bool:
         percentage_failures = self._get_percentage_failures(
             total_failures=total_failures,
             total_requests=total_requests
@@ -36,7 +40,9 @@ class PercentageFailuresRule(BaseRule):
             percentage_failures >= self.max_failures_percentage
         )
 
-    def log_increase_failures(self, total_failures, total_requests):
+    def log_increase_failures(
+        self, total_failures: int, total_requests: int
+    ) -> None:
         percentage_failures = self._get_percentage_failures(
             total_failures=total_failures,
             total_requests=total_requests
