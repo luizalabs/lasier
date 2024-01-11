51c2aaf5d52c03d98c3cf0fed72579ed441479c4
Anand Kumar <akr.cpibtc@gmail.com>
2024-01-11 21:03:26 +0530

    Added aioredis adapter

---
+++ lasier/adapters/caches/aioredis.py
@@ -0,0 +1,8 @@
+from lasier.types import Timeout
+
+from .base import AsyncCacheAdapterBase
+
+
+class Adapter(AsyncCacheAdapterBase):
+    async def add(self, key: str, value: int, timeout: Timeout = None) -> None:
+        self.cache.set(key, value, timeout, nx=True)
